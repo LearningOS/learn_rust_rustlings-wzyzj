5ba86eabb74142ff5c79ced2505826d1cba933fb
wzyzj <80394817+wzyzj@users.noreply.github.com>
2022-11-05 14:45:24 +0000

    update

--- exercises/option/option2.rs
+++ exercises/option/option2.rs
@@ -6,9 +6,8 @@ fn main() {
     let optional_word = Some(String::from("rustlings"));
 
     // TODO: Make this an if let statement whose value is "Some" type
-    let word = Some(String::from("rustlings"));
-    if word == optional_word {
-        println!("The word is: {}", word.is_some());
+    if let Some(word) = optional_word {
+        println!("The word is: {}", word);
     } else {
         println!("The optional word doesn't contain anything");
     }
@@ -20,8 +19,7 @@ fn main() {
 
     // TODO: make this a while let statement - remember that vector.pop also adds another layer of Option<T>
     // You can stack `Option<T>`'s into while let and if let
-    let integer:Option<i8> = Some(10);
-    if let integer = optional_integers_vec.pop() {
-        println!("current value: {}", integer.is_some());
+    if let Some(Some(integer)) = optional_integers_vec.pop() {
+        println!("current value: {}", integer);
     }
 }
